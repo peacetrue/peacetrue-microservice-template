1f1581d8f39f07cbf36544cbb74035c18e99e6b7
xiayx <xiayouxue@gold32.com>
2020-07-23 05:47:51 +0800

    集成 sleuth

--- restarts.sh
+++ restarts.sh
@@ -2,6 +2,7 @@
 
 env="prod"
 
+peacetrue-microservice-zipkin/restart4prod.sh
 ./restart.sh peacetrue-microservice-config-center 8888 "$env" && sleep 20s
 ./restart.sh peacetrue-microservice-registry-center 8761 "$env" && sleep 20s
 #./restart.sh peacetrue-microservice-authorization-server 8510 "$env"
@@ -9,3 +10,4 @@ env="prod"
 ./restart.sh peacetrue-microservice-client-inner 8530 "$env"
 ./restart.sh peacetrue-microservice-gateway 443 "$env"
 ./restart.sh peacetrue-microservice-client-outer 8531 "$env"
+
