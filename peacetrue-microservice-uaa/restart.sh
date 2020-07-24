5da48c155e5fd7db6d498389685ca9aed63aca49
xiayx <xiayouxue@gold32.com>
2020-07-24 17:10:01 +0800

    finished version  local/1.3.0/spring-boot-admin

--- peacetrue-microservice-uaa/restart.sh
+++ peacetrue-microservice-uaa/restart.sh
@@ -6,15 +6,17 @@ env=${1-prod}
 echo "0. 启动[$env]环境 uaa 服务"
 
 basePath=$(pwd)
-httpsPath=""
+logPath="$basePath"
+configPath="$basePath"
 
 killbp 8080
 killbp 8443
 killbp 8005
 
-[ "$env" == "prod" ] && basePath="$basePath/prod" && httpsPath="/https"
-echo "$basePath$httpsPath"
-JAVA_OPTS="-server -Xmx256m -Xms256m -Xmn96m -Xss256k -Dlogging.config=$basePath/log4j2.properties -DUAA_CONFIG_PATH=$basePath$httpsPath" apache-tomcat-9.0.37/bin/startup.sh
+[ "$env" == "prod" ] && logPath="$basePath/prod" && configPath="$basePath/prod"
+[ "$env" == "4prod" ] && logPath="$basePath" && configPath="$basePath/prod"
+echo "$logPath | $configPath"
+JAVA_OPTS="-server -Xmx256m -Xms256m -Xmn96m -Xss256k -Dlogging.config=$basePath/log4j2.properties -DUAA_CONFIG_PATH=$basePath" apache-tomcat-9.0.37/bin/startup.sh
 
 # http://localhost:8080/uaa/
 # https://peacetrue.cn:8443/uaa/
