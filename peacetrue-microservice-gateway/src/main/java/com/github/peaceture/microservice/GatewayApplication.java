556ff571831d0c28ebd5dade10ca57bab2cc4058
xiayx <xiayouxue@gold32.com>
2020-09-01 08:24:41 +0800

    set GATEWAY_SESSION

--- peacetrue-microservice-gateway/src/main/java/com/github/peaceture/microservice/GatewayApplication.java
+++ peacetrue-microservice-gateway/src/main/java/com/github/peaceture/microservice/GatewayApplication.java
@@ -1,11 +1,15 @@
 package com.github.peaceture.microservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.web.server.adapter.HttpWebHandlerAdapter;
+import org.springframework.web.server.session.CookieWebSessionIdResolver;
+import org.springframework.web.server.session.DefaultWebSessionManager;
 
 /**
  * @author 安宁
@@ -23,6 +27,17 @@ public class GatewayApplication {
     @Profile("https")
     public AutoLifecycleWebServer autoLifecycleWebServer(HttpHandler httpHandler) {
         return new AutoLifecycleWebServer(httpHandler);
+    }
+
+    @Autowired(required = false)
+    public void configHttpHandler(HttpHandler httpHandler) {
+        if (httpHandler == null) return;
+        if (!(httpHandler instanceof HttpWebHandlerAdapter)) return;
+        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+        CookieWebSessionIdResolver sessionIdResolver = new CookieWebSessionIdResolver();
+        sessionIdResolver.setCookieName("GATEWAY_SESSION");
+        sessionManager.setSessionIdResolver(sessionIdResolver);
+        ((HttpWebHandlerAdapter) httpHandler).setSessionManager(sessionManager);
     }
 
 //    /** 如果启用 https，同时启动一个 http 服务，重定向到 https */
