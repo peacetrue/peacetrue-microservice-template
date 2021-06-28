67ef02203285be60e8a0069a61b266a323935333
xiayx <xiayouxue@gold32.com>
2021-06-29 05:24:09 +0800

    clean up

--- peacetrue-microservice-zipkin/restart.sh
+++ peacetrue-microservice-zipkin/restart.sh
@@ -5,7 +5,8 @@ env=${1-prod}
 echo "0. 启动[$env]环境 zipkin 服务"
 killbp 9411
 [ "$env" == "prod" ] && host="peacetrue.cn" || host="localhost"
-STORAGE_TYPE=elasticsearch ES_HOSTS="http://$host:9200/" ES_USERNAME=elastic ES_PASSWORD=password nohup java -Xmx256m -Xms256m -Xmn96m -Xss256k -jar zipkin.jar &
+#STORAGE_TYPE=elasticsearch ES_HOSTS="http://$host:9200/" ES_USERNAME=elastic ES_PASSWORD=password nohup java -Xmx256m -Xms256m -Xmn96m -Xss256k -jar zipkin.jar &
+nohup java -Xmx256m -Xms256m -Xmn96m -Xss256k -jar zipkin.jar &
 
 # http://localhost:9411/zipkin/
 # http://peacetrue.cn:9411/zipkin/
