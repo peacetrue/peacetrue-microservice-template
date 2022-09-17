c7a44889c3c93d2e78ab8530cba07beb4d3069f7
peacetrue <xiayouxue@gmail.com>
2022-09-17 12:44:14 +0800

    remove unused import

--- peacetrue-microservice-config-center/src/main/java/com/github/peaceture/microservice/ConfigCenterApplication.java
+++ peacetrue-microservice-config-center/src/main/java/com/github/peaceture/microservice/ConfigCenterApplication.java
@@ -1,16 +1,8 @@
 package com.github.peaceture.microservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-import org.springframework.boot.actuate.health.HealthEndpoint;
-import org.springframework.boot.actuate.info.InfoEndpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author 安宁
